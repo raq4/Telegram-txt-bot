1873f5f10d7ade18d490e347769332d12ddc6043
raq4 <gover8004@gmail.com>
2026-01-17 17:45:40 +0300

    Update and rename Bot.py to bot.py

--- bot.py
+++ bot.py
@@ -6,10 +6,10 @@ TOKEN = os.environ["BOT_TOKEN"]
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(func=lambda m: m.text and not m.text.startswith('/'))
-def make_txt(message):
-    file = io.BytesIO(message.text.encode('utf-8'))
-    file.name = 'text.txt'
-    bot.send_document(message.chat.id, file)
+def work(m):
+    f = io.BytesIO(m.text.encode())
+    f.name = 'text.txt'
+    bot.send_document(m.chat.id, f)
 
-print("Бот работает!")
+print("Bot started!")
 bot.infinity_polling()
